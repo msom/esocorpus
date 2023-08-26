8ea7883ae96b962be13b71fb140752fb1d78f912
Marc Sommerhalder <msom@gmx.ch>
2023-08-26 11:49:55 +0200

    Cleanup Key to Theosophy

--- data-processed/esocorpus.R
+++ data-processed/esocorpus.R
@@ -77,6 +77,27 @@ cleanup_secret_doctrine <- function(row) {
   }
 }
 
+cleanup_key_to_theosophy <- function() {
+  #'
+  #' Cleanup Secret Doctrine Vol. 1-3
+  #'
+  #' There seems to be no hyphenated words, only double-words such as
+  #' "self-denial".
+  #'
+  #' There are some paragraphs spanning over multiple new lines, we fix only
+  #' the ones continuing with lower case letters.
+
+  file <- "data-processed/Blavatsky_Helena Petrovna_1889_The Key to Theosophy.txt"
+  text <- readChar(file, file.info(file)$size) %>%
+    str_replace_all("\\(\\d*\\)", "") %>% # footnote signs
+    str_replace_all("(.{1})‐\\n(.{1})", "\\1-\\2")  %>% # collapse paragraphs with hyphenation
+    str_replace_all("(.{1})\\n(.{1})", "\\1 \\2")  %>% # collapse paragraphs
+    str_replace_all("(.{1})\\n    (.{1})", "\\1 \\2") %>% # collapse blockquotes
+    str_replace_all("([a-z][-]?)\\n\\n([a-z])", "\\1 \\2") # paragraphs spanning over pages
+
+  cat(text, file=file)
+}
+
 # ------------------------------------------------------------------------------
 # Read files and store as text files
 # ------------------------------------------------------------------------------
@@ -102,6 +123,7 @@ rm(esocorpus_raw)
 # ------------------------------------------------------------------------------
 cleanup_isis_unveiled()
 cleanup_secret_doctrine()
+cleanup_key_to_theosophy()
 
 # FIXME: remove_hathitrust_metadata
 # Cahagnet_Louis Alphonse_1855_The Celestial Telegraph
